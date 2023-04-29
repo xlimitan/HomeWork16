9df80c0e38703332b60b3be7e2699cce7cbe5cdd
xlimitan <xlimitan@yandex.ru>
2023-04-29 12:13:43 +0300

    2

--- src/main/java/com/homework16/homework16/Homework16Application.java
+++ src/main/java/com/homework16/homework16/Homework16Application.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Homework16Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(Homework16Application.class, args);
-	}
 
+		SpringApplication.run(Homework16Application.class, args);
+
+	}
 }
---
+++ src/main/java/com/homework16/homework16/WrongPassException.java
@@ -0,0 +1,22 @@
+package com.homework16.homework16;
+
+public class WrongPassException extends  Exception {
+    public WrongPassException() {
+    }
+
+    public WrongPassException(String message) {
+        super(message);
+    }
+
+    public WrongPassException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public WrongPassException(Throwable cause) {
+        super(cause);
+    }
+
+    public WrongPassException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
