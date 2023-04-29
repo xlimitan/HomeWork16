7ae12c08ec70a017202998010605dae3fcba4a8d
xlimitan <xlimitan@yandex.ru>
2023-04-29 10:02:33 +0300

    Initial commit

---
+++ src/test/java/com/homework16/homework16/Homework16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework16.homework16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
